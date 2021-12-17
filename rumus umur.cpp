99e472924ef50e1a669ce77c7eb445954becec91
Santi putri <96229820+santiputri02@users.noreply.github.com>
2021-12-17 16:32:46 +0700

    Add files via upload

---
+++ rumus umur.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+main()
+{
+	int nomermenu;
+	float tahunlahir, tahunsekarang, umur;
+	
+	{
+		cout<<"tahunsekarang : "; cin>>tahunsekarang;
+        cout<<"tahunlahir 	 : "; cin>>tahunlahir;
+        umur=tahunsekarang-tahunlahir;
+		cout<<"umur   : "<<umur<<endl;
+	}
+}
